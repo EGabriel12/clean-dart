b8be9ea2354ed2a7e6a0ad89cf48813904912814
Elias Gabriel <elias.gabrielfn12@gmail.com>
2021-03-09 22:12:15 -0400

    feat: add first test that should return a ResultSearch list

--- cleandart/lib/modules/search/domain/usecases/search_by_text.dart
+++ cleandart/lib/modules/search/domain/usecases/search_by_text.dart
@@ -1,7 +1,20 @@
 import 'package:cleandart/modules/search/domain/entities/result_search.dart';
 import 'package:cleandart/modules/search/domain/errors/errors.dart';
+import 'package:cleandart/modules/search/domain/repositories/search_repository.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class SearchByText {
   Future<Either<FailureSearch, List<ResultSearch>>> call(String searchText);
+}
+
+class SearchByImpl implements SearchByText {
+  final SearchRepository repository;
+
+  SearchByImpl(this.repository);
+
+  @override
+  Future<Either<FailureSearch, List<ResultSearch>>> call(
+      String searchText) async {
+    return repository.search(searchText);
+  }
 }
