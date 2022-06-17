8cfac20e6c13febc9a5e99ab961faff5b24fd7a3
elias gabriel <eliasgab@motorola.com>
2022-06-16 22:53:02 -0400

    feat: add domain layer

---
+++ lib/modules/search/domain/entities/reasult_search_entity.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ResultSearchEntity {
+  final String title;
+  final String content;
+  final String image;
+  ResultSearchEntity({
+    required this.title,
+    required this.content,
+    required this.image,
+  });
+}
